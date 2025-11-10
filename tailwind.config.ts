9b33aff47319535bd8e99afe9161eabe6edbce2f
jenish <jenishkhakhriya@gmail.com>
2025-11-10 22:18:12 +0530

    fixed install tailwind with tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ff0000", // Example custom color
+        secondary: "#9333EA",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
